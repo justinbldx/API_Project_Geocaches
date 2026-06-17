329f28e76f4d8c07b77d46ffae484c6e7e4bcf74
justinbldx <justinblondiaux354@gmail.com>
2026-06-17 17:51:42 +0200

    feat: Ajoute des requêtes API pour les réseaux

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,30 @@
 import { Router } from 'express';
 import { UserController } from './controllers/user.controller';
 import { asyncHandler } from './middlewares/asyncHandler';
+import { NetworkController } from './controllers/network.controller';
 
 const router = Router();
 const userController = new UserController();
+const networkController = new NetworkController();
 
-router.get('/', asyncHandler(userController.getAll));
-router.get('/:id', asyncHandler(userController.getById));
-router.post('/', asyncHandler(userController.create));
-router.put('/:id', asyncHandler(userController.update));
-router.delete('/:id', asyncHandler(userController.delete));
+// router.get('/', asyncHandler(userController.getAll));
+// router.get('/:id', asyncHandler(userController.getById));
+// router.post('/', asyncHandler(userController.create));
+// router.put('/:id', asyncHandler(userController.update));
+// router.delete('/:id', asyncHandler(userController.delete));
+
+// Network routes
+router.get('/networks', asyncHandler(networkController.getAll));
+router.get('/networks/:id', asyncHandler(networkController.getById));
+router.post('/networks', asyncHandler(networkController.create));
+router.put('/networks/:id', asyncHandler(networkController.update));
+router.delete('/networks/:id', asyncHandler(networkController.delete));
+router.get('/networks/:id/members', asyncHandler(networkController.getMembers));
+router.post('/networks/:id/members', asyncHandler(networkController.addMember));
+router.delete(
+  '/networks/:network_id/members/:member_id',
+  asyncHandler(networkController.removeMember)
+);
+router.get('/networks/:id/caches', asyncHandler(networkController.getCaches));
 
 export default router;
